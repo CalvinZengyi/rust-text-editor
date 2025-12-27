49c44718fcff640e786bf9174733fd1621e5b404
Yi Zeng <calvin.zengyi@gmail.com>
2025-12-26 23:23:31 -0800

    move semantic and ownership

--- hecto/src/editor.rs
+++ hecto/src/editor.rs
@@ -1,8 +1,9 @@
 use crossterm::event::{Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers, read};
+use std::io::Error;
 
 mod terminal;
 
-use terminal::Terminal;
+use terminal::{Position, Size, Terminal};
 
 pub struct Editor {
     should_quit: bool,
@@ -20,7 +21,7 @@ impl Editor {
         result.unwrap();
     }
 
-    fn repl(&mut self) -> Result<(), std::io::Error> {
+    fn repl(&mut self) -> Result<(), Error> {
         loop {
             self.refresh_scree()?;
             if self.should_quit {
@@ -46,23 +47,27 @@ impl Editor {
         }
     }
 
-    fn refresh_scree(&self) -> Result<(), std::io::Error> {
+    fn refresh_scree(&self) -> Result<(), Error> {
+        Terminal::hide_cursor()?;
         if self.should_quit {
-            Terminal::clear_scree()?;
-            println!("Goodbye. \r\n");
+            Terminal::clear_screen()?;
+            Terminal::print("Goodbye. \r\n")?;
         } else {
             Self::draw_rows()?;
-            Terminal::move_cursor_to(0, 0)?;
+            Terminal::move_cursor_to(Position { x: 0, y: 0 })?;
         }
+        Terminal::show_cursor()?;
+        Terminal::execute()?;
         Ok(())
     }
 
     fn draw_rows() -> Result<(), std::io::Error> {
-        let height = Terminal::size()?.1;
+        let Size{height, ..} = Terminal::size()?;
         for current_row in 0..height {
-            print!("~");
+            Terminal::clean_line()?;
+            Terminal::print("~")?;
             if current_row + 1 < height {
-                print!("\n\r");
+                Terminal::print("\n\r")?;
             }
         }
         Ok(())
